ec7729ae769fb68658bb9da225ea20c52cde6ea7
Arthur de Sa <arthurdesa94@gmail.com>
2020-07-15 13:58:00 -0500

    ex 2 add

---
+++ exercises_4_3/ex2.js
@@ -0,0 +1,11 @@
+exibirAsteriscos(4);
+
+    function exibirAsteriscos(linhas){
+    
+    let padrao = "";
+        for(let linha = 1; linha <= linhas; linha++){
+            padrao += "*"
+            console.log(padrao);
+        }
+
+    }
